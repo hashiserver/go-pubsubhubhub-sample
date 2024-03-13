9fb702ee9b7b44ee76b351538ee45a4e9fce12b6
Ren hashimoto <hashimotoren@team-lab.com>
2024-03-13 16:27:19 +0900

    検証

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,16 +11,17 @@ func main() {
 	// Ginエンジンのインスタンスを作成
 	r := gin.Default()
 
-	// ルートURL ("/") に対するGETリクエストをハンドル
 	r.GET("/feed", func(c *gin.Context) {
 		// JSONレスポンスを返す
 		hubChallenge := c.Query("hub.challenge")
+		log.Println("hub.challenge", hubChallenge)
 		c.String(200, hubChallenge)
 	})
 
 	r.POST("/feed", func(c *gin.Context) {
 		log.Println("POST")
 		log.Println(c.Request.Body)
+		c.Status(http.StatusNoContent)
 	})
 
 	// 8080ポートでサーバーを起動
